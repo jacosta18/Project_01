186c635d7b628c7c081364a3033351c39aa04890
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-07-15 20:33:20 +0100

    first commit

--- Python_Homework_Practise_1/01_Homework.py
+++ Python_Homework_Practise_1/01_Homework.py
@@ -1,5 +1,17 @@
-product_a = 12
-product_b = 3
-total_sales = product_a*product_b
+#Exersice 3
 
-print(total_sales)  
+story = {
+    'Hero': 'There was once a hero called Sparticus.',
+    'Introduction':'He had a brother, Glactus, they were both brave, cunning and stubborn',
+    'Beginning': 'His wife, Dany, was take from him by his soon to be king, Glactus.',
+    'Middle':"He formed an army of rebels, 3000 strong and marched towards the capital and fought outside the city's gates.",
+    'End': 'Both Glactus and Sparticus fell on the battlefield, making Dany queen of the entire kingdom.',
+    'Sub_End': "\U0001F602"
+}
+
+print(story['Hero'])
+print(story['beginning'])
+print(story['middle'])
+print(story['End'])
+print(story['Sub_End'])
+
