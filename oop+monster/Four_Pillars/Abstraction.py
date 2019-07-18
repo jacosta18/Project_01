af5c7e902a8238fcf3e4b8257f0f2f1a885ddad4
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-07-18 15:37:17 +0100

    Abstraction

--- oop+monster/Four_Pillars/Abstraction.py
+++ oop+monster/Four_Pillars/Abstraction.py
@@ -1,1 +1,57 @@
 #### The weakest points ###
+
+# Given the user only the information needed to perform a task
+# While keeping unnecesary information away
+# Using python modules as an exampleof abstraction
+
+
+
+# Four pillars of Object Oriented Programming
+# Inheriatance
+
+##### MOST FUNDAMENTAL POINT #####
+
+class Animaml():  ## super class of truck
+
+    def __init__(self,age, weight, specie):
+        self.age = age      # same as age = 15 in oop
+        self.weight= weight
+        self.specie = specie
+
+    def can_hunt(self,value):
+       return value
+
+    def eat(self):
+       return "yum yum yum"
+
+    def sleep(self):
+       return "zzzzzzzzz"
+
+class Mammal(Animaml):
+    def __init__(self,age, weight, specie,name,can_speak):
+        super().__init__(age,weight, specie)
+        self.name = name
+        self.can_speak = can_speak
+
+    def can_speak_language(self,can_speak):
+        return can_speak
+
+class Bird (Animaml):
+    def __init__(self,age, weight, specie,name,feather_color):
+        super().__init__(age,weight, specie)
+        self.name = name
+        self.feather_color = feather_color
+
+    def can_fly(self,can_fly):
+        return can_fly
+
+class Reptiles (Animaml):
+    def __init__(self, age, weight, specie, name, region_found):
+        super(). __init__(age,weight,specie)
+        self.name = name
+        self.region_found = region_found
+
+    def is_venomous(self,value):
+        return value
+
+
---
+++ oop+monster/Four_Pillars/Using abstraction.py
@@ -0,0 +1,25 @@
+from Four_Pillars.Abstraction import Reptiles
+
+class Snake(Reptiles):
+    def __init__(self, age, weight,specie,name,region_found,number_of_fangs,length):
+        super().__init__(age, weight,specie,name,region_found)
+        self.number_of_fangs = number_of_fangs
+        self.length = length
+
+    def can_constrict(self,bool_value):
+        return bool_value
+
+    def can_swim(self,bool_value):
+        return bool_value
+
+python =Snake(35,450,"pythoras terroriza","Bop","Morgate",0,12)
+
+print(python.length)
+print(python.specie)
+print(python.region_found)
+print(python.number_of_fangs)
+print(python.age)
+print(python.can_constrict(True))
+
+
+
