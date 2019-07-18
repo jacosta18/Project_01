50de8e97c5b7da15c45ba3e3a3478322aad687e9
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-07-18 11:09:36 +0100

    OOP start

---
+++ oop+monster/01_OOP.py
@@ -0,0 +1,50 @@
+
+class Monster:
+
+    def __init__(self, name='', age=0, rating=0):
+        self.skills = []
+        self.name = name
+        self.national_insurance = ''
+        self.age = int(age)
+        self.scary_rating = rating
+
+    def sleep(self):
+        return 'zzzz'
+
+    def eat(self):
+        return 'nom nom'
+
+    def scare_attack(self):
+        return 'RAAAHHH'
+
+    def add_skill(self, skill):
+        # we have a skill passed in the arguments
+        # We want to append this skill to our monster's(this perticular instance of monster)
+            #skills attribute
+        self.skills.append(skill)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        def shout_out_skills_in_interview(self):
+            skills_list = self.skills
+            for skills 
+
+
+
---
+++ oop+monster/02_workshop.py
@@ -0,0 +1,15 @@
+
+
+
+class Workshop():
+
+    def_init_('self')
+    def_init_(self, name_wshp, teacher = 'Not assaigned', attendees): # parameter or how it looks like
+        self.workshop_name = ''
+        self.workshop_teacher = teacher
+        self.monster_attendees_list = attendees
+
+
+
+
+
