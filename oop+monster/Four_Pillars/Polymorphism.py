26556211388f68772f9f3785f6f58591c071e13c
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-07-18 16:57:38 +0100

    encapsulation recap

--- oop+monster/Four_Pillars/Polymorphism.py
+++ oop+monster/Four_Pillars/Polymorphism.py
@@ -1,1 +1,19 @@
+# One thing changing in many ways
+# Method polymorphism
+
+
+# When a method is behaving differently depending on the data or number
+# parameters passed to it
+# Overload method
+
+
+# Inheritance [polymorphism]
+## When a sub class can take the place of it's supper class and respond
+## to message call
+
+## Override method
+
+###### CREATE A SUPER CLASS england usa inherit from COUNTRY
+
+#Country capital language type
 
